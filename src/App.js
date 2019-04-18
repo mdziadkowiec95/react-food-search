57f81686d0cd3a09c30e5c6857b36c08533482ad
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-04-18 22:20:48 +0200

    added first serach input

--- src/App.js
+++ src/App.js
@@ -4,43 +4,17 @@ import Search from './components/Search';
 
 class App extends Component {
   state = {
-    geolocationCoords: '',
-  }
 
-  componentDidMount() {
-    if (navigator.geolocation) {
-      const watcher = navigator.geolocation.getCurrentPosition(this.displayLocationInfo, this.handleLocationError);
-    }
-  }
-
-  displayLocationInfo = (position) => {
-    const lng = position.coords.longitude;
-    const lat = position.coords.latitude;
-
-    this.setState({
-      geolocationCoords: [lng, lat],
-    })
-  }
-
-  handleLocationError = (error) => {
-    if (error.code) {
-      this.setState({
-        geolocationCoords: '',
-        geoErrorMessage: error.message
-      })
-    }
   }
 
   render() {
-    if (this.state.geolocationCoords) {
-      return (
-        <div className="App">
-          <Search coords={this.state.geolocationCoords} />
-        </div>
-      );
-    } else {
-      return <p>has not geo</p>
-    }
+
+    return (
+      <div className="App">
+        <Search />
+      </div>
+    );
+
   }
 }
 
