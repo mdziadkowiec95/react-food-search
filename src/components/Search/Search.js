581f56d46d96637efd874722649dbd86f133b33b
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-05-30 21:09:22 +0200

    moved Serach methods from Root to Serach component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,33 +7,385 @@ import Select from '../Select/Select';
 import AppContext from '../../AppContext';
 import Autocomplete from './Autocomplete';
 import cx from 'classnames';
-// import cities from 'cities.json';
+import { withRouter } from 'react-router';
+import Modal from '../../Modal';
+import { MdClose } from 'react-icons/md';
+import GlobalLoader from '../Base/GlobalLoader';
+import { debounce } from 'underscore';
+import Pagination from '../Pagination/Pagination';
 
 const API_KEY = process.env.REACT_APP_FOOD_API_KEY;
 
-const citiesEndpoint =
-  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const API_REQ_HEADER = {
+  method: 'GET',
+  headers: new Headers({
+    'user-key': API_KEY
+  })
+};
 
-class Search extends React.Component {
+/** helper functions */
+const normalizeString = str =>
+  str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace('ł', 'l'); // @toDo --- temporary
+
+/** Component */
+class SearchBase extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      globalLoader: false,
+      loadingGeolocation: false,
+      geolocationCoords: {},
+      queryCity: '',
+      cityAutocompleteMatches: [],
+      cityData: {},
+      restaurantsCountStart: 0,
+      restaurants: [],
+      cuisines: [],
+      categories: [],
+      categoryID: '',
+      showModal: false,
+      modalText: ''
+    };
+
+    this.handleCityChangeDebounced = debounce(this.getCityData, 200);
+  }
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    fetch(`https://developers.zomato.com/api/v2.1/categories`, API_REQ_HEADER)
+      .then(data => data.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          categories: res.categories
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  getGeolocation = () => {
+    if (navigator.geolocation) {
+      this.setState({
+        loadingGeolocation: true
+      });
+
+      const watcher = navigator.geolocation.getCurrentPosition(
+        this.displayLocationInfo,
+        this.handleLocationError
+      );
+    }
+  };
+
+  displayLocationInfo = position => {
+    this.setState(
+      {
+        geolocationCoords: {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude
+        }
+      },
+      this.getCityName
+    );
+  };
+
+  handleLocationError = error => {
+    if (error.code) {
+      if (error.code === 2) {
+        this.setState({
+          geolocationCoords: {},
+          showModal: true,
+          modalText: 'You need to allow geolocation in device browser settings',
+          loadingGeolocation: false
+        });
+      } else {
+        this.setState({
+          geolocationCoords: {},
+          showModal: true,
+          modalText: 'You need to allow geolocation in device browser settings',
+          loadingGeolocation: false
+        });
+      }
+    }
+  };
+
+  getCityName = () => {
+    fetch(
+      `https://developers.zomato.com/api/v2.1/geocode?lat=${
+        this.state.geolocationCoords.latitude
+      }&lon=${this.state.geolocationCoords.longitude}`,
+      API_REQ_HEADER
+    )
+      .then(data => data.json())
+      .then(res => {
+        this.setState({
+          queryCity: res.location.city_name,
+          cityData: {
+            name: res.location.city_name,
+            id: res.location.city_id
+          },
+          loadingGeolocation: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleCityChange = e => {
+    this.setState({
+      queryCity: e.target.value
+    });
+
+    this.handleCityChangeDebounced();
+  };
+
+  handleCategoryChange = event => {
+    const ID = event.target[event.target.selectedIndex].dataset.id;
+
+    this.setState({
+      categoryID: ID
+    });
+  };
+
+  handleCuisineChange = e => {
+    const ID = e.target[e.target.selectedIndex].dataset.id;
+
+    this.setState({
+      cuisineID: ID
+    });
+  };
+
+  getCityData = isFormSubmited => {
+    if (this.state.queryCity.length >= 2) {
+      console.log(normalizeString(this.state.queryCity));
+
+      fetch(
+        `https://developers.zomato.com/api/v2.1/cities?q=${normalizeString(
+          this.state.queryCity
+        )}`,
+        API_REQ_HEADER
+      )
+        .then(data => data.json())
+        .then(res => {
+          const matchedLocations = res.location_suggestions;
+
+          if (!isFormSubmited) {
+            if (matchedLocations.length > 0) {
+              const matches = matchedLocations.map(el => {
+                return {
+                  name: el.name,
+                  id: el.id
+                };
+              });
+
+              this.setState({
+                cityAutocompleteMatches: matches
+              });
+            } else {
+              this.setState({
+                cityAutocompleteMatches: []
+              });
+            }
+          } else {
+            if (matchedLocations.length > 0) {
+              this.setState(
+                {
+                  queryCity: matchedLocations[0].name,
+                  cityData: {
+                    name: matchedLocations[0].name,
+                    id: matchedLocations[0].id
+                  }
+                },
+                this.sendSearchRequest
+              );
+            } else {
+              this.setState({
+                globalLoader: false,
+                showModal: true,
+                modalText: 'No matches for provided City'
+              });
+            }
+          }
+        })
+        .catch(err => {
+          this.setState({
+            globalLoader: false,
+            showModal: true,
+            modalText: err
+          });
+        });
+    } else {
+      this.setState({
+        cityAutocompleteMatches: []
+      });
+    }
+  };
+
+  setCity = (e, isClickedOutside) => {
+    if (!isClickedOutside) {
+      this.setState({
+        queryCity: e.target.dataset.name,
+        cityAutocompleteMatches: []
+      });
+    } else {
+      this.setState({
+        cityAutocompleteMatches: []
+      });
+    }
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.queryCity) {
+      this.setState(
+        {
+          globalLoader: true
+        },
+        /** getCityData(isFormSubmited) ---> boolean to inform that for has been submited */
+        this.getCityData(true)
+      );
+    } else {
+      this.setState({
+        showModal: true,
+        modalText: `You need to enter any city`
+      });
+    }
+  };
+
+  sendSearchRequest = (countStart = 0) => {
+    const cuisineQuery = this.state.cuisineID
+      ? `&cuisines=${this.state.cuisineID}`
+      : '';
+    const categoryQuery = this.state.categoryID
+      ? `&category=${this.state.categoryID}`
+      : '';
+
+    if (this.state.cityData.id) {
+      fetch(
+        `https://developers.zomato.com/api/v2.1/search?entity_id=${
+          this.state.cityData.id
+        }&entity_type=city${cuisineQuery + categoryQuery}&start=${countStart}`,
+        API_REQ_HEADER
+      )
+        .then(data => data.json())
+        .then(res => {
+          console.log(res);
+          const restaurantsArr = res.restaurants.map(item => {
+            const restaurant = item.restaurant;
+
+            return {
+              id: restaurant.id,
+              name: restaurant.name,
+              city: restaurant.location.city,
+              address: restaurant.location.address,
+              area: restaurant.location.locality_verbose,
+              img: restaurant.thumb
+                ? restaurant.thumb
+                : restaurant.featured_image,
+              cuisines: restaurant.cuisines,
+              currency: restaurant.currency,
+              costForTwo: restaurant.average_cost_for_two,
+              ratingNumber: restaurant.user_rating.agerate_rating,
+              ratingColor: restaurant.user_rating.rating_color,
+              ratingText: restaurant.user_rating.rating_text,
+              votes: restaurant.user_rating.votes
+            };
+          });
+
+          if (restaurantsArr.length > 0) {
+            this.setState(
+              {
+                globalLoader: false,
+                restaurantsCountStart: countStart
+              },
+              this.props.context.setFetchedRestaurants(restaurantsArr)
+            );
+
+            if (this.props.history.location.pathname !== '/')
+              this.props.history.push('/');
+          } else {
+            this.setState({
+              globalLoader: false,
+              showModal: true,
+              modalText: 'No matches found for provided data'
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      this.setState({
+        globalLoader: false,
+        modalText: 'No matches found for provided City',
+        showModal: true
+      });
+    }
+  };
+
+  navigateThroughResults = (e, direction) => {
+    const newCount =
+      direction === 'next'
+        ? this.state.restaurantsCountStart + 20
+        : this.state.restaurantsCountStart - 20;
+
+    if (newCount >= 0) {
+      this.setState(
+        {
+          globalLoader: true
+        },
+        this.sendSearchRequest(newCount)
+      );
+    }
+  };
+
+  closeModal = () => {
+    this.setState({
+      queryCity: '',
+      modalText: '',
+      showModal: false
+    });
+  };
+
   render() {
+    const {
+      globalLoader,
+      loadingGeolocation,
+      queryCity,
+      cityAutocompleteMatches,
+      categories,
+      showModal,
+      modalText
+    } = this.state;
+
     return (
       <AppContext.Consumer>
         {context => (
           <>
+            {globalLoader && <GlobalLoader />}
             <div className={styles.wrapper}>
               <form
                 method="POST"
                 action=""
                 className={styles.form}
-                onSubmit={e => context.handleFormSubmit(e, this.props.history)}
+                onSubmit={this.handleFormSubmit}
               >
                 <div className={cx(styles.formGroup, styles.formGroupCity)}>
                   <button
                     type="button"
-                    onClick={context.getGeolocation}
+                    onClick={this.getGeolocation}
                     className={styles.btnGeolocation}
                   >
-                    {!context.loadingGeolocation ? (
+                    {!loadingGeolocation ? (
                       <MdNearMe />
                     ) : (
                       <span className={styles.btnGeolocationLoader}>
@@ -41,33 +393,64 @@ class Search extends React.Component {
                       </span>
                     )}
                   </button>
-                  {/* {console.log(context)} */}
                   <input
                     type="text"
                     placeholder="Enter city name..."
-                    value={context.queryCity}
-                    onChange={context.handleCityChange}
+                    value={queryCity}
+                    onChange={this.handleCityChange}
+                    disabled={loadingGeolocation}
                     className={cn(styles.formInput, styles.formInputLocation)}
                   />
-                  {context.cityAutocompleteMatches.length > 0 && (
-                    <Autocomplete items={context.cityAutocompleteMatches} />
+                  {cityAutocompleteMatches.length > 0 && (
+                    <Autocomplete
+                      items={cityAutocompleteMatches}
+                      setCityFn={this.setCity}
+                    />
                   )}
                 </div>
                 <div className={styles.selectWrapper}>
                   <Select
                     dataType="categories"
-                    onChangeFn={context.handleCategoryChange}
-                    items={context.categories}
+                    onChangeFn={this.handleCategoryChange}
+                    items={categories}
                   />
                 </div>
                 <Button submit>Search</Button>
               </form>
             </div>
+
+            {this.props.history.location.pathname === '/' &&
+            this.props.context.restaurants.length > 0 ? (
+              <Pagination paginationFn={this.navigateThroughResults} />
+            ) : null}
+
+            {showModal && (
+              <Modal>
+                <div className={styles.modal}>
+                  <div className={styles.modalInner}>
+                    <button
+                      className={styles.modalButton}
+                      onClick={this.closeModal}
+                    >
+                      <MdClose size={30} color="#fff" />
+                    </button>
+                    <h3 className={styles.modalText}>{modalText}</h3>
+                  </div>
+                </div>
+              </Modal>
+            )}
           </>
         )}
       </AppContext.Consumer>
     );
   }
 }
+const SearchWithRouter = withRouter(SearchBase);
+
+const Search = props => (
+  <AppContext.Consumer>
+    {context => <SearchWithRouter context={context} />}
+  </AppContext.Consumer>
+);
 
 export default Search;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -12,10 +12,9 @@ import SignInView from '../SignInView/SignInView';
 import AccountView from '../AccountView/AccountView';
 import PasswordForgetView from '../PasswordForgetView/PasswordForgetView';
 import Modal from '../../Modal';
-import GlobalLoader from '../../components/Base/GlobalLoader';
 import { debounce } from 'underscore';
-import { MdClose } from 'react-icons/md';
 import { restaurantsTEST } from '../../testData';
+import Search from '../../components/Search/Search';
 
 const API_KEY = process.env.REACT_APP_FOOD_API_KEY;
 const API_REQ_HEADER = {
@@ -31,33 +30,12 @@ class Root extends Component {
 
     this.state = {
       authUser: null,
-      globalLoader: false,
-      loadingGeolocation: false,
-      geolocationCoords: {},
-      queryCity: '',
-      cityAutocompleteMatches: [],
-      cityData: {},
-      restaurantsCountStart: 0,
       restaurants: [],
-      cuisines: [],
-      categories: [],
-      categoryID: '',
       isSidebarOpen: false,
       isNavOpen: false,
       showModal: false,
-      modalText: 'font-size: 16px; font-size: 16px;',
-      handleCityChange: this.handleCityChange,
-      navigateThroughResults: this.navigateThroughResults,
-      setCity: this.setCity,
-      handleCategoryChange: this.handleCategoryChange,
-      handleCuisineChange: this.handleCuisineChange,
-      handleFormSubmit: this.handleFormSubmit,
-      getGeolocation: this.getGeolocation,
-      handleToggleSidebar: this.handleToggleSidebar,
-      handleToggleNav: this.handleToggleNav
+      modalText: 'font-size: 16px; font-size: 16px;'
     };
-
-    this.handleCityChangeDebounced = debounce(this.getCityData, 200);
   }
 
   componentDidMount() {
@@ -69,315 +47,15 @@ class Root extends Component {
           : this.setState({ authUser: null });
       }
     );
-    this.getCategories();
   }
 
   componentWillUnmount() {
     this.listenerAuth();
   }
 
-  componentDidMount() {
-    this.getCategories();
-  }
-
-  getCategories = () => {
-    fetch(`https://developers.zomato.com/api/v2.1/categories`, API_REQ_HEADER)
-      .then(data => data.json())
-      .then(res => {
-        console.log(res);
-        this.setState({
-          categories: res.categories
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  getGeolocation = () => {
-    if (navigator.geolocation) {
-      this.setState({
-        loadingGeolocation: true
-      });
-
-      const watcher = navigator.geolocation.getCurrentPosition(
-        this.displayLocationInfo,
-        this.handleLocationError
-      );
-    }
-  };
-
-  displayLocationInfo = position => {
-    this.setState(
-      {
-        geolocationCoords: {
-          longitude: position.coords.longitude,
-          latitude: position.coords.latitude
-        }
-      },
-      this.getCityName
-    );
-  };
-
-  handleLocationError = error => {
-    if (error.code) {
-      if (error.code === 2) {
-        this.setState({
-          geolocationCoords: {},
-          showModal: true,
-          modalText: 'You need to allow geolocation in device browser settings',
-          loadingGeolocation: false
-        });
-      } else {
-        this.setState({
-          geolocationCoords: {},
-          showModal: true,
-          modalText: 'You need to allow geolocation in device browser settings',
-          loadingGeolocation: false
-        });
-      }
-    }
-  };
-
-  getCityName = () => {
-    fetch(
-      `https://developers.zomato.com/api/v2.1/geocode?lat=${
-        this.state.geolocationCoords.latitude
-      }&lon=${this.state.geolocationCoords.longitude}`,
-      API_REQ_HEADER
-    )
-      .then(data => data.json())
-      .then(res => {
-        this.setState({
-          queryCity: res.location.city_name,
-          cityData: {
-            name: res.location.city_name,
-            id: res.location.city_id
-          },
-          loadingGeolocation: false
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  handleCityChange = e => {
+  setFetchedRestaurants = restaurantsArr => {
     this.setState({
-      queryCity: e.target.value
-    });
-
-    this.handleCityChangeDebounced();
-  };
-
-  handleCategoryChange = event => {
-    const ID = event.target[event.target.selectedIndex].dataset.id;
-
-    this.setState({
-      categoryID: ID
-    });
-  };
-
-  handleCuisineChange = e => {
-    const ID = e.target[e.target.selectedIndex].dataset.id;
-
-    this.setState({
-      cuisineID: ID
-    });
-  };
-
-  getCityData = getFirstMatch => {
-    if (this.state.queryCity.length >= 2) {
-      const queryNormalized = this.state.queryCity
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace('ł', 'l'); // @toDo --- temporary
-
-      console.log(queryNormalized);
-
-      fetch(
-        `https://developers.zomato.com/api/v2.1/cities?q=${queryNormalized}`,
-        API_REQ_HEADER
-      )
-        .then(data => data.json())
-        .then(res => {
-          const matchedLocations = res.location_suggestions;
-
-          if (!getFirstMatch) {
-            if (matchedLocations.length > 0) {
-              const matches = matchedLocations.map(el => {
-                return {
-                  name: el.name,
-                  id: el.id
-                };
-              });
-              console.log(matches);
-
-              this.setState({
-                cityAutocompleteMatches: matches
-              });
-            } else {
-              this.setState({
-                cityAutocompleteMatches: []
-              });
-            }
-          } else {
-            if (matchedLocations.length > 0) {
-              this.setState(
-                {
-                  queryCity: matchedLocations[0].name,
-                  cityData: {
-                    name: matchedLocations[0].name,
-                    id: matchedLocations[0].id
-                  }
-                },
-                this.sendSearchRequest
-              );
-            } else {
-              this.setState({
-                showModal: true,
-                modalText: 'No matches for provided City',
-                globalLoader: false
-              });
-            }
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      this.setState({
-        cityAutocompleteMatches: []
-      });
-    }
-  };
-
-  setCity = (event, isClickedOutside, id, name) => {
-    if (!isClickedOutside) {
-      this.setState({
-        queryCity: name,
-        cityAutocompleteMatches: []
-      });
-    } else {
-      this.setState({
-        cityAutocompleteMatches: []
-      });
-    }
-  };
-
-  handleFormSubmit = (e, history) => {
-    e.preventDefault();
-
-    history.push('/');
-
-    if (this.state.queryCity) {
-      this.setState({
-        globalLoader: true
-      });
-
-      this.getCityData(true);
-    } else {
-      this.setState({
-        showModal: true,
-        modalText: `You need to enter any city`
-      });
-    }
-  };
-
-  sendSearchRequest = (countStart = 0) => {
-    const cuisineQuery = this.state.cuisineID
-      ? `&cuisines=${this.state.cuisineID}`
-      : '';
-    const categoryQuery = this.state.categoryID
-      ? `&category=${this.state.categoryID}`
-      : '';
-
-    console.log(
-      `https://developers.zomato.com/api/v2.1/search?entity_id=${
-        this.state.cityData.id
-      }&entity_type=city${cuisineQuery + categoryQuery}&start=${countStart}`
-    );
-
-    if (this.state.cityData.id) {
-      fetch(
-        `https://developers.zomato.com/api/v2.1/search?entity_id=${
-          this.state.cityData.id
-        }&entity_type=city${cuisineQuery + categoryQuery}&start=${countStart}`,
-        API_REQ_HEADER
-      )
-        .then(data => data.json())
-        .then(res => {
-          console.log(res);
-          const restaurantsArr = res.restaurants.map(item => {
-            const restaurant = item.restaurant;
-
-            return {
-              id: restaurant.id,
-              name: restaurant.name,
-              city: restaurant.location.city,
-              address: restaurant.location.address,
-              area: restaurant.location.locality_verbose,
-              img: restaurant.thumb
-                ? restaurant.thumb
-                : restaurant.featured_image,
-              cuisines: restaurant.cuisines,
-              currency: restaurant.currency,
-              costForTwo: restaurant.average_cost_for_two,
-              ratingNumber: restaurant.user_rating.agerate_rating,
-              ratingColor: restaurant.user_rating.rating_color,
-              ratingText: restaurant.user_rating.rating_text,
-              votes: restaurant.user_rating.votes
-            };
-          });
-
-          if (restaurantsArr.length > 0) {
-            this.setState({
-              globalLoader: false,
-              restaurantsCountStart: countStart,
-              restaurants: restaurantsArr
-            });
-          } else {
-            this.setState({
-              globalLoader: false,
-              // restaurantsCountStart: countStart,
-              showModal: true,
-              modalText: 'No matches found for provided data'
-            });
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      this.setState({
-        globalLoader: false,
-        modalText: 'No matches found for provided City',
-        showModal: true
-      });
-    }
-  };
-
-  navigateThroughResults = (e, direction) => {
-    const newCount =
-      direction === 'next'
-        ? this.state.restaurantsCountStart + 20
-        : this.state.restaurantsCountStart - 20;
-
-    if (newCount >= 0) {
-      this.setState(
-        {
-          globalLoader: true
-        },
-        this.sendSearchRequest(newCount)
-      );
-    }
-  };
-
-  closeModal = () => {
-    this.setState({
-      queryCity: '',
-      modalText: '',
-      showModal: false
+      restaurants: restaurantsArr
     });
   };
 
@@ -394,11 +72,17 @@ class Root extends Component {
   };
 
   render() {
+    const AppContextElements = {
+      ...this.state,
+      setFetchedRestaurants: this.setFetchedRestaurants,
+      handleToggleNav: this.handleToggleNav,
+      handleToggleSidebar: this.handleToggleSidebar
+    };
     return (
       <BrowserRouter>
-        <AppContext.Provider value={this.state}>
+        <AppContext.Provider value={AppContextElements}>
           <div className="App">
-            {this.state.globalLoader && <GlobalLoader />}
+            <Search />
             <Navigation />
             {<Sidebar delayTime={300} isMounted={this.state.isSidebarOpen} />}
             <Switch>
@@ -409,22 +93,6 @@ class Root extends Component {
               <Route path="/password-forget" component={PasswordForgetView} />
               <Route path="/restaurant/:id" component={RestaurantView} />
             </Switch>
-
-            {this.state.showModal && (
-              <Modal>
-                <div className={styles.modal}>
-                  <div className={styles.modalInner}>
-                    <button
-                      className={styles.modalButton}
-                      onClick={this.closeModal}
-                    >
-                      <MdClose size={30} color="#fff" />
-                    </button>
-                    <h3 className={styles.modalText}>{this.state.modalText}</h3>
-                  </div>
-                </div>
-              </Modal>
-            )}
           </div>
         </AppContext.Provider>
       </BrowserRouter>
