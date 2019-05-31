61f437cd3b546c16318c20f877322798dca4dd76
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-05-31 18:12:17 +0200

    added bigger z-index to Pagination

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,19 +1,5 @@
 import React from 'react';
 
-const AppContext = React.createContext({
-  geolocationCoords: {},
-  queryCity: '',
-  cityData: {},
-  restaurants: [],
-  cuisines: [],
-  categories: [],
-  categoryID: '',
-  cuisineID: '',
-  handleCityChange() {},
-  handleCategoryChange() {},
-  handleCuisineChange() {},
-  handleFormSubmit() {},
-  getGeolocation() {}
-});
+const AppContext = React.createContext({});
 
 export default AppContext;
